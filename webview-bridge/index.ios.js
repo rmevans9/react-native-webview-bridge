79783ddb212f63da7e1c39bdf78cddc6a614bb43
Richard Evans <rmevans9@gmail.com>
2016-01-13 07:04:51 -0500

    Fix odd iOS npm include issues.

--- webview-bridge/index.ios.js
+++ webview-bridge/index.ios.js
@@ -15,8 +15,21 @@
 'use strict';
 
 var React = require('react-native');
-var invariant = require('invariant');
-var keyMirror = require('keymirror');
+var invariant = React.invariant;
+
+var keyMirror = function(obj) {
+  var ret = {};
+  var key;
+  if (!(obj instanceof Object && !Array.isArray(obj))) {
+    throw new Error('keyMirror(...): Argument must be an object.');
+  }
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      ret[key] = key;
+    }
+  }
+  return ret;
+};
 
 var {
   ActivityIndicatorIOS,
