b58b2b3ca1b329d08a898a1152ff184041def27b
Richard Evans <rmevans9@gmail.com>
2016-01-18 19:51:38 -0500

    Revert change that actually was not needed

--- webview-bridge/index.ios.js
+++ webview-bridge/index.ios.js
@@ -15,21 +15,8 @@
 'use strict';
 
 var React = require('react-native');
-var invariant = React.invariant;
-
-var keyMirror = function(obj) {
-  var ret = {};
-  var key;
-  if (!(obj instanceof Object && !Array.isArray(obj))) {
-    throw new Error('keyMirror(...): Argument must be an object.');
-  }
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      ret[key] = key;
-    }
-  }
-  return ret;
-};
+var invariant = require('invariant');
+var keyMirror = require('keymirror');
 
 var {
   ActivityIndicatorIOS,
